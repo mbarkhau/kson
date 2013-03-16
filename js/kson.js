07eecc7d7366e93eaeff55069fa69af85d5ffacd
Manuel Barkhau <mbarkhau@gmail.com>
2013-03-16 15:08:02 +0100

    remove feebsd project reference

--- js/kson.js
+++ js/kson.js
@@ -23,10 +23,6 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  *
- * The views and conclusions contained in the software and documentation are
- * those of the authors and should not be interpreted as representing official
- * policies, either expressed or implied, of the FreeBSD Project.
- *
  *
  * KSON: Keyless Schemafied Object Notation
  *
